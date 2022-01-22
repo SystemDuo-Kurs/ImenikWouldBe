7256e26a971b4c717b8d116d018fb2ada923102f
Dragan Pepic <nedimco2006@gmail.com>
2022-01-22 16:33:35 +0100

    asdasd

--- Program.cs
+++ Program.cs
@@ -1,4 +1,15 @@
-﻿bool izlaz = false;
+﻿int broj = 27;
+do
+{
+    Console.WriteLine(broj);
+    if (broj % 2 == 0)
+        broj /= 2;
+    else
+        broj = broj * 3 + 1;
+} while (broj > 1);
+Console.WriteLine(broj);
+
+/*bool izlaz = false;
 do
 {
     StampajMeni();
@@ -32,4 +43,4 @@ void StampajMeni()
     Console.WriteLine("~~2. Prikaz osoba~~");
     Console.WriteLine("~~3. Izlaz~~");
     Console.WriteLine("*********************");
-}
+}*/
